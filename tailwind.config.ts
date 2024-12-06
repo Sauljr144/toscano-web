3a80e53d7f73f57344c1b08304cfc477fb1d54da
Sauljr144 <saulsuazojr06@gmail.com>
2024-12-06 13:28:11 -0800

    Initial commit.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,23 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'poppins': ['"Poppins"', 'sans-serif'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'white-tan':'#FFFCF1',
+        'red':'#D55242',
+        'green':'#D2E37C',
+        'orange': '#FFDC91',
+        'orange-light': '#FFEABD',
+        'orange-dark': '#FF8C00',
+        'tan': '#FAF3D1',
+        'tan-dark': '#FFECCC',
+        'yellow': '#FBF496',
+        'blue-light': '#F5F3F3',
+
       },
     },
   },
